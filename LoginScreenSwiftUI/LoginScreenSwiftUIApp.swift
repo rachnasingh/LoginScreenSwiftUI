197e0fc5c9580f7712b36e195b968766876c8cec
Rachna Singh <engineerachna@gmail.com>
2025-07-08 21:01:34 +0530

    Initial Commit

---
+++ LoginScreenSwiftUI/LoginScreenSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenSwiftUIApp.swift
+//  LoginScreenSwiftUI
+//
+//  Created by Rachna Singh on 08/07/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
